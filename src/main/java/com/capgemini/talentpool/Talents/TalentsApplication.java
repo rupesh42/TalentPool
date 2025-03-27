c9c669ae04aadc917f2d761cc646b0025d331238
Sukale <rupesh.sukale@capgemini.com>
2025-03-27 12:51:14 +0100

    Initial commit

---
+++ src/main/java/com/capgemini/talentpool/Talents/TalentsApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.talentpool.Talents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/talentpool/Talents/TalentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.talentpool.Talents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
